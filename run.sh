0bf5d7876748339dd2470a0c39d43a1f8bb36fb2
Hajime Hoshi <hajimehoshi@gmail.com>
2021-11-06 00:43:35 +0900

    Remove unneeded slash

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ mkdir -p /tmp/malloc_test
 
 runtime_dir=$(go list -f {{.Dir}} runtime)
 echo "{\"Replace\":{
-    \"$runtime_dir/mem_darwin.go\":\"$(pwd)/overlay/mem_darwin.go/\"
+    \"$runtime_dir/mem_darwin.go\":\"$(pwd)/overlay/mem_darwin.go\"
 }}" > /tmp/malloc_test/overlay.json
 go run -overlay /tmp/malloc_test/overlay.json .
 
