11840b32b36dfc665ecadd306d5cb7ca341e5077
Hajime Hoshi <hajimehoshi@gmail.com>
2021-11-07 01:32:26 +0900

    Skip a test

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ runtime_dir=$(go list -f {{.Dir}} runtime)
 echo "{\"Replace\":{
     \"$runtime_dir/mem_darwin.go\":\"$(pwd)/overlay/mem_darwin.go\",
     \"$runtime_dir/sys_darwin.go\":\"$(pwd)/overlay/sys_darwin.go\",
-    \"$runtime_dir/sys_darwin_amd64.s\":\"$(pwd)/overlay/sys_darwin_amd64.s\"
+    \"$runtime_dir/sys_darwin_amd64.s\":\"$(pwd)/overlay/sys_darwin_amd64.s\",
+    \"$runtime_dir/malloc_test.go\":\"$(pwd)/overlay/malloc_test.go\"
 }}" > /tmp/malloc_test/overlay.json
 go run -overlay /tmp/malloc_test/overlay.json .
 
