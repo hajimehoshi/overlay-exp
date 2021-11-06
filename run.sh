6d4b17bbc865f27ff5cac69716578cba05df7f0b
Hajime Hoshi <hajimehoshi@gmail.com>
2021-11-07 01:36:11 +0900

    Move Go files

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@ mkdir -p /tmp/malloc_test
 
 runtime_dir=$(go list -f {{.Dir}} runtime)
 echo "{\"Replace\":{
-    \"$runtime_dir/mem_darwin.go\":\"$(pwd)/overlay/mem_darwin.go\",
-    \"$runtime_dir/sys_darwin.go\":\"$(pwd)/overlay/sys_darwin.go\",
-    \"$runtime_dir/sys_darwin_amd64.s\":\"$(pwd)/overlay/sys_darwin_amd64.s\",
-    \"$runtime_dir/malloc_test.go\":\"$(pwd)/overlay/malloc_test.go\"
+    \"$runtime_dir/mem_darwin.go\":\"$(pwd)/overlay/runtime/mem_darwin.go\",
+    \"$runtime_dir/sys_darwin.go\":\"$(pwd)/overlay/runtime/sys_darwin.go\",
+    \"$runtime_dir/sys_darwin_amd64.s\":\"$(pwd)/overlay/runtime/sys_darwin_amd64.s\",
+    \"$runtime_dir/malloc_test.go\":\"$(pwd)/overlay/runtime/malloc_test.go\"
 }}" > /tmp/malloc_test/overlay.json
 go run -overlay /tmp/malloc_test/overlay.json .
 
